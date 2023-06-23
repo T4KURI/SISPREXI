fb0a581071e544d368abb67dcc4956560824f793
Carlos Alvarez <137438250+T4KURI@users.noreply.github.com>
2023-06-23 00:33:21 -0500

    Add: EliminarFonograma lambda function

---
+++ src/lambda/EliminarFonograma.py
@@ -0,0 +1,16 @@
+import pymysql
+
+def lambda_handler(event, context):
+    
+    conn = pymysql.connect(host = 'host_url',
+                           user = 'user',
+                           password = 'password',
+                           database = 'database',
+                           port = 3306)
+    cur = conn.cursor()
+    cur.execute('''DELETE FROM Interprete_Fonograma WHERE IdFonograma = ('%s')''' % (event['Id']))
+    conn.commit()
+    cur.execute('''DELETE FROM Fonograma WHERE IdFonograma = ('%s')''' % (event['Id']))
+    conn.commit()
+    cur.close()
+    conn.close()
