15a0024bcab4dffebabaa24768bf0b0a1c74fdb1
Carlos Alvarez <137438250+T4KURI@users.noreply.github.com>
2023-06-23 00:25:26 -0500

    Add: BuscarInterpreteFonograma lambda function

---
+++ src/lambda/BuscarInterpreteFonograma.py
@@ -0,0 +1,16 @@
+import pymysql
+import json
+
+def lambda_handler(event, context):
+    
+    conn = pymysql.connect(host = 'host_url',
+                           user = 'user',
+                           password = 'password',
+                           database = 'database',
+                           port = 3306)
+    cur = conn.cursor()
+    cur.execute('''SELECT * FROM Interprete_Fonograma WHERE IdFonograma = ('%s')''' % (event['Id_Fonograma']))
+    resultado = cur.fetchall()
+    cur.close()
+    conn.close()
+    return json.dumps(resultado, default=str)
