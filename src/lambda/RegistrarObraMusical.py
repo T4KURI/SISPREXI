d0e032bf92ac8eb6af3f1896423aad0f591fca55
Carlos Alvarez <137438250+T4KURI@users.noreply.github.com>
2023-07-08 13:47:33 -0500

    Update RegistrarObraMusical.py

--- src/lambda/RegistrarObraMusical.py
+++ src/lambda/RegistrarObraMusical.py
@@ -1,19 +1,52 @@
 import pymysql
 import json
+import boto3
+import json
 
 def lambda_handler(event, context):
     
-    conn = pymysql.connect(host = 'host_url',
-                       user = 'user',
-                       password = 'password',
-                       database = 'database',
+    conn = pymysql.connect(host = 'database-mysql.c3ecu4syhg5e.us-east-1.rds.amazonaws.com',
+                       user = 'admin',
+                       password = 'admin123',
+                       database = 'PRY20220223',
                        port = 3306)
     cur = conn.cursor()
     cur.execute('''INSERT INTO Obra_Musical(Titulo, Tipo_Obra) VALUES ('%s', '%s')''' % (event['titulo'], event['tipoobra']))
     conn.commit()
     obra_musical = cur.lastrowid
-    cur.execute('''INSERT INTO Letra_Musical(Letra, IdObra_Musical, IdTipo_Sentimiento) VALUES ('%s', '%s', '%s')''' % (event['titulo'], obra_musical, 2))
-    conn.commit()
+    
+    #AnalisisDeSentimiento
+    
+    s3 = boto3.client("s3")
+    bucket = "letrasmusicales"
+    key = event['titulo']+".txt"
+    
+    file = s3.get_object(Bucket = bucket, Key = key)
+    contenido = str(file['Body'].read())
+    
+    comprehend = boto3.client("comprehend")
+    response = comprehend.detect_sentiment(Text = contenido, LanguageCode = "es")
+    
+    lista = json.loads(json.dumps(response, default=str))
+    
+    if(lista["Sentiment"] == "POSITIVE"):
+        cur.execute('''INSERT INTO Letra_Musical(Letra, IdObra_Musical, IdTipo_Sentimiento) VALUES ('%s', '%s', '%s')''' % (key, obra_musical, 1))
+        conn.commit()
+    
+    if(lista["Sentiment"] == "NEGATIVE"):
+        cur.execute('''INSERT INTO Letra_Musical(Letra, IdObra_Musical, IdTipo_Sentimiento) VALUES ('%s', '%s', '%s')''' % (key, obra_musical, 2))
+        conn.commit()
+    
+    if(lista["Sentiment"] == "MIXED"):
+        cur.execute('''INSERT INTO Letra_Musical(Letra, IdObra_Musical, IdTipo_Sentimiento) VALUES ('%s', '%s', '%s')''' % (key, obra_musical, 3))
+        conn.commit()
+    
+    else:
+        cur.execute('''INSERT INTO Letra_Musical(Letra, IdObra_Musical, IdTipo_Sentimiento) VALUES ('%s', '%s', '%s')''' % (key, obra_musical, 4))
+        conn.commit()
+        
+    #print(lista["Sentiment"])
+    
     cur.close()
     conn.close()
     return json.dumps(obra_musical, default=str)
